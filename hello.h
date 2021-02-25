cd38bd2343c2004fbb9f72928beb46bfd49b394a
bebebe <beatakychkina@yandex.ru>
2021-02-25 22:43:31 +0300

    teamlead commit

---
+++ hello.h
@@ -0,0 +1,9 @@
+#ifndef HELLO_H
+#define HELLO_H
+#include<iostream>
+
+void print_kbl1();
+void print_kbl2();
+void print_kbl3();
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include"hello.h"
+
+int main() {
+        print_kbl1();
+	print_kbl2();
+	print_kbl3();
+        return 0;
+}
+
